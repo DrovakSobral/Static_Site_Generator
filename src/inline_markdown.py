2efc7b5dd0da58738fe1f4615c6b0137b6de7c40
João Manoel Sobral da Costa <joaomsobral.drovak@gmail.com>
2025-10-25 19:10:45 -0300

    Added 2 functions that take as input a list of TextNodes and extract images and links from them. They make use of the previously added functions to extract images and links from raw markdown strings

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -33,3 +33,53 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
+
+
+def split_nodes_image(old_nodes: list[TextNode]):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        raw_images = extract_markdown_images(old_node.text)
+        if raw_images == []:
+            new_nodes.append(old_node)
+            continue
+        else:
+            old_text = old_node.text
+            for raw_image in raw_images:
+                img_alt_txt = raw_image[0]
+                img_link = raw_image[1]
+                split_text = old_text.split(f"![{img_alt_txt}]({img_link})", 1)
+                if split_text[0] != "":
+                    new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                new_nodes.append(TextNode(img_alt_txt, TextType.IMAGE, img_link))
+                old_text = split_text[1]
+            if old_text != "":
+                new_nodes.append(TextNode(old_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes: list[TextNode]):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        raw_links = extract_markdown_links(old_node.text)
+        if raw_links == []:
+            new_nodes.append(old_node)
+            continue
+        else:
+            old_text = old_node.text
+            for raw_link in raw_links:
+                link_txt = raw_link[0]
+                link_url = raw_link[1]
+                split_text = old_text.split(f"[{link_txt}]({link_url})", 1)
+                if split_text[0] != "":
+                    new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                new_nodes.append(TextNode(link_txt, TextType.LINK, link_url))
+                old_text = split_text[1]
+            if old_text != "":
+                new_nodes.append(TextNode(old_text, TextType.TEXT))
+    return new_nodes
