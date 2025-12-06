377fc25e7afd1a1c9bbc8c1fee9995d529229aa4
João Manoel Sobral da Costa <joaomsobral.drovak@gmail.com>
2025-12-06 15:49:40 -0300

    Added the test_textnode.py file, which contains tests for the textnode.py file. Also added the test.sh script to make it easier to run all tests at once.

---
+++ tests/test_textnode.py
@@ -0,0 +1,14 @@
+import pytest
+
+from textnode import TextNode, TextType
+
+
+def test_eq_textnode():
+    node = TextNode("This is a text node", TextType.BOLD)
+    node2 = TextNode("This is a text node", TextType.BOLD)
+    assert node == node2
+
+def test_not_eq_textnode():
+    node = TextNode("This is a bold text node", TextType.BOLD)
+    node2 = TextNode("This is an italic text node", TextType.ITALIC)
+    assert node != node2
