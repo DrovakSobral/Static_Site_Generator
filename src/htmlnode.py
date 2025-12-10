7c162b2137abe1234dc21a9469cda4ae47be26be
João Manoel Sobral da Costa <joaomsobral.drovak@gmail.com>
2025-12-09 21:41:10 -0300

    Added the ParentNode class to htmlnode.py and new tests related to the new class to test_htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,10 +16,10 @@ class HTMLNode:
 
     def props_to_html(self) -> str:
         try:
-            result = ""
+            prop_html = ""
             for prop in self.props:
-                result += f' {prop}="{self.props[prop]}"'
-            return result
+                prop_html += f' {prop}="{self.props[prop]}"'
+            return prop_html
         except TypeError:
             return ""
 
@@ -35,12 +35,35 @@ class LeafNode(HTMLNode):
         props: dict[str, str] = None,
     ) -> LeafNode:
         super().__init__(tag, value, None, props)
-    
+
     def to_html(self):
         if self.value is None:
-            raise ValueError("The LeafNode has no value!")
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
         if self.tag == "img":
             return f"<{self.tag}{self.props_to_html()}>"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(TAG = {self.tag}, VALUE = {self.value}, PROPS = {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self, tag: str, children: list[HTMLNode], props: dict[str, str] = None
+    ):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Invalid HTML: no tag")
+        if not self.children:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode(TAG = {self.tag}, CHILDREN = {self.children}, PROPS = {self.props})"
--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -1,5 +1,5 @@
 import pytest
-from src.htmlnode import HTMLNode, LeafNode
+from src.htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 def test_props_to_html_none():
@@ -13,18 +13,18 @@ def test_props_to_html_empty():
 
 
 def test_props_to_html_multiple_pairs():
-    node = HTMLNode(props={"href" : "https://www.google.com"})
+    node = HTMLNode(props={"href": "https://www.google.com"})
     assert node.props_to_html() == ' href="https://www.google.com"'
 
 
 def test_props_to_html_multiple_pairs():
-    node = HTMLNode(props={"href" : "https://www.google.com", "target": "_blank"})
+    node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
     assert node.props_to_html() == ' href="https://www.google.com" target="_blank"'
 
 
 def test_leafnode_to_html_no_value():
-    node = LeafNode(None, None)
-    with pytest.raises(ValueError):
+    node = LeafNode("p", None)
+    with pytest.raises(ValueError, match="Invalid HTML: no value"):
         node.to_html()
 
 
@@ -39,10 +39,54 @@ def test_leafnode_to_html_p():
 
 
 def test_leafnode_to_html_link():
-    node = LeafNode("a", "Lorem ipsum", {"href":"https://www.boot.dev"})
+    node = LeafNode("a", "Lorem ipsum", {"href": "https://www.boot.dev"})
     assert node.to_html() == '<a href="https://www.boot.dev">Lorem ipsum</a>'
 
 
 def test_leafnode_to_html_img():
-    node = LeafNode("img", "", {"src":"url/of/img.jpg", "alt":"description of img"})
+    node = LeafNode("img", "", {"src": "url/of/img.jpg", "alt": "description of img"})
     assert node.to_html() == '<img src="url/of/img.jpg" alt="description of img">'
+
+
+def test_parentnode_to_html_tag_none():
+    node = ParentNode(None, None, None)
+    with pytest.raises(ValueError, match="Invalid HTML: no tag"):
+        node.to_html()
+
+
+def test_parentnode_to_html_tag_empty_string():
+    node = ParentNode("", None, None)
+    with pytest.raises(ValueError, match="Invalid HTML: no tag"):
+        node.to_html()
+
+
+def test_parentnode_to_html_children_none():
+    node = ParentNode("div", None, None)
+    with pytest.raises(ValueError, match="Invalid HTML: no children"):
+        node.to_html()
+
+
+def test_parentnode_to_html_children_empty_list():
+    node = ParentNode("div", [], None)
+    with pytest.raises(ValueError, match="Invalid HTML: no children"):
+        node.to_html()
+
+
+def test_parentnode_to_html_children_one():
+    child_node = LeafNode("span", "child")
+    parent_node = ParentNode("div", [child_node])
+    assert parent_node.to_html() == "<div><span>child</span></div>"
+
+
+def test_parentnode_to_html_children_multiple():
+    child_node_1 = LeafNode("span", "child_1")
+    child_node_2 = LeafNode("b", "child_2")
+    parent_node = ParentNode("div", [child_node_1, child_node_2])
+    assert parent_node.to_html() == "<div><span>child_1</span><b>child_2</b></div>"
+
+
+def test_parentnode_to_html_grandchildren():
+    grandchild_node = LeafNode("b", "grandchild")
+    child_node = ParentNode("span", [grandchild_node])
+    parent_node = ParentNode("div", [child_node])
+    assert parent_node.to_html() == "<div><span><b>grandchild</b></span></div>"
